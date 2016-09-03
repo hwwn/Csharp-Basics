f9232d946c8019a2f784f0b70f067cbdeef1dc91
hwwn <hwwn@live.com>
2016-09-03 20:38:12 +0800

    After Refactoring

--- C# Basics.cs
+++ C# Basics.cs
@@ -6,64 +6,62 @@ namespace Treehouse.FitnessFrog
 	{
 		static void Main()
 		{
-			double runningTotal = 0;
-			bool keepGoing = true;
+			var runningTotal = 0.0;
 
-			while(keepGoing)
+			while(true)
 			{
 				//Prompt the user for minutes exercised
 				Console.Write("Enter how many minutes you exercised or type \"quit\" to exit: ");
-				string entry = Console.ReadLine();
+				var entry = Console.ReadLine();
 
 				if(entry.ToLower() == "quit")
 				{
 					break;
 				}
-				else
+				
+				try
 				{
-					try
+					var minutes = double.Parse(entry);
+					
+					if(minutes <= 0)
 					{
-						double minutes = double.Parse(entry);
-						
-						if(minutes <= 0)
-						{
-							Console.WriteLine(minutes + " is not an acceptable value.");
-							continue;
-						}
-						else if(minutes <= 10)
-						{
-							Console.WriteLine("Better than nothing, am I right?");
-						}
-						else if(minutes <= 30)
-						{
-							Console.WriteLine("Way to go hot stuff!");
-						}
-						else if(minutes <= 60)
-						{
-							Console.WriteLine("You must be a ninja warrior in training!");
-						}
-						else
-						{
-							Console.WriteLine("Okay, now you're just showing off!");
-						}
-					}
-
-					catch(FormatException)
-					{
-						Console.WriteLine("That is not valid input.");
+						Console.WriteLine(minutes + " is not an acceptable value.");
 						continue;
 					}
-
-					
-
-				//Add minutes exercised to total
-				runningTotal = runningTotal + minutes;
-
-				//Display total minutes exercised to the screen
-				Console.WriteLine("You've entered " + runningTotal +" minutes");
+					else if(minutes <= 10)
+					{
+						Console.WriteLine("Better than nothing, am I right?");
+					}
+					else if(minutes <= 30)
+					{
+						Console.WriteLine("Way to go hot stuff!");
+					}
+					else if(minutes <= 60)
+					{
+						Console.WriteLine("You must be a ninja warrior in training!");
+					}
+					else
+					{
+						Console.WriteLine("Okay, now you're just showing off!");
+					}
 				}
+
+				catch(FormatException)
+				{
+					Console.WriteLine("That is not valid input.");
+					continue;
+				}
+
 				
-				//Repeat until the user quits
+
+			//Add minutes exercised to total
+			runningTotal += minutes;
+
+			//Display total minutes exercised to the screen
+			Console.WriteLine("You've entered " + runningTotal +" minutes");
+			
+			
+			//Repeat until the user quits
 
 			}
 
